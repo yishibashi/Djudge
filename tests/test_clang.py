948c933b9776860e772876b0b48fe46443251183
Eshin Kunishima <ek@esh.ink>
2016-10-26 17:19:38 +0900

    Can compile C++ string without any handling...lol

--- tests/test_clang.py
+++ tests/test_clang.py
@@ -1,5 +1,13 @@
+import pytest
+
 from djudge.clang import Clang
+from djudge.exceptions import CompileError
 
 
 def test_compile():
     Clang.compile('int main() {return 0;}')
+
+
+def test_compile_with_wrong_code():
+    with pytest.raises(CompileError):
+        Clang.compile('clang')
