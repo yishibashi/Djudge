53c4ae018b30f0ef7e8052945650ba407f6b32a5
yishibashi <yishibashi101@gmail.com>
2016-11-12 23:36:05 +0900

    動くってだけでクソ

--- djudge/app.py
+++ djudge/app.py
@@ -1,11 +1,12 @@
 import sys
 import hug
 import pkgutil
-
+from djudge.db import *
 
 @hug.get('/', output=hug.output_format.html)
 def index_get():
     return pkgutil.get_data(__package__, 'templates/index.html').decode()
+
 
 @hug.get('/submit', output=hug.output_format.html)
 def submit_get():
@@ -13,16 +14,36 @@ def submit_get():
 
 @hug.post('/submit', output=hug.output_format.json)
 def submit_post(problem: int, compiler: int, code: hug.types.text):
-    return {
-        'problem': problem,
-        'compiler': compiler,
-        'code': code
+    print(problem, compiler, code)
+    p= Problem(problem, compiler, code)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(p)
+    session.commit()
+    return{
+    'problem': problem,
+    'compiler': compiler,
+    'code': code
     }
 
 @hug.get('/contribute', output=hug.output_format.html)
 def contribute():
     return pkgutil.get_data(__package__, 'templates/contribute.html').decode()
 
+
+@hug.post('/contribute', output=hug.output_format.json)
+def contribute(problem, compiler, code):
+    print(problem, compiler, code)
+    p= Problem(problem, compiler, code)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.add(p)
+    session.commit()
+    return{
+    'problem': problem,
+    'compiler': compiler,
+    'code': code
+    }
 
 
 if __name__ == '__main__':
--- djudge/db.py
+++ djudge/db.py
@@ -1,5 +1,6 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///db.sqlite3', echo=True)
 metadata = MetaData()
@@ -16,16 +17,23 @@ class Problem(Base):
     problem_type = Column(String(32), nullable=False)
     problem_body = Column(Text, nullable=False) # Text? String?
 
-    def __init__(self, content_name, problem_type, problem_body):
-        self.contest_name = content_name
+    def __init__(self, contest_name, problem_type, problem_body):
+        self.contest_name = contest_name
         self.problem_type = problem_type
         self.problem_body = problem_body
 
+class Submit(Base):
+    __tablename__ = 'submit'
 
+    id = Column(Integer, primary_key=True)
+    problem = Column(String(32), nullable=False)
+    compiler = Column(String(32), nullable=False)
+    code = Column(Text, nullable=False)
 
+    def __init__(self, problem, compiler, code):
+        self.problem = problem
+        self.compiler = compiler
+        self.code = code
 
-
-
-
-
-
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
